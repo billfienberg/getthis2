186f9f8ce38a7d96660ae94bd6fc2bb379f03711
Drew Robinson <drewbie0307@hotmail.com>
2014-02-22 14:22:58 -0600

    Got User database hooked up to list DB

--- app.rb
+++ app.rb
@@ -8,57 +8,75 @@ require './models.rb'
 
 get "/" do
   @title = "GetThis2"
-  erb :index
+  erb :signin
 end
 
 post "/" do
   @title = "GetThis2"
 
-  redirect "user/#{user.id}"
+  if User.exists?(:username => params[:username])
+    @user = User.find_by(username: params[:username])
+    redirect "user/#{@user.id}"
+  else
+    @user = User.create(username: params[:username], password: params[:password])
+    redirect "user/#{@user.id}"
+  end
 end
 
-get "user/:id" do
-  
+get "/user/:id" do
+  @title = "Welcome User"
+  @user = User.find(params[:id])
+
+  erb :"/user/index"
 end
 
-get "/lists" do
+get "/user/:id/lists" do
   @title = "Your Lists"
+  @user = User.find(params[:id])
 
   @all_lists = []
   List.all.each do |x|
-    @all_lists << x
+    if x.user_id == @user.id
+      @all_lists << x
+    end
   end
 
   erb :lists
 end
 
-get "/lists/new" do
+get "/user/:id/lists/new" do
   @title = "New List"
-   
-  erb :"/lists/make_list" 
+  @user = User.find(params[:id])
+
+  erb :"/user/lists/make_list" 
 end
 
-post "/lists/new" do
-  @list = List.create(name: params[:listname], keyword: params[:keyword])
+post "/user/:id/lists/new" do
+  @title = "New List"
+  @user = User.find(params[:id])
+  @list = List.create(user_id: @user.id, name: params[:listname], keyword: params[:keyword])
 
-  redirect "lists/#{@list.id}" 
+  redirect "user/lists/#{@list.id}" 
 end
 
-get "/lists/:id" do
+get "/user/lists/:id" do
+  @title = "My List"
   @list = List.find(params[:id])
   @title = "Add items"
   
-  erb :"/lists/add_items"
+  erb :"/user/lists/add_items"
 end
 
-post "/lists/:id" do
+post "/user/lists/:id" do
+  @title = "Add Items"
   @list = List.find(params[:id])
   @item = Item.create(list_id: @list.id, name: params[:itemname], quantity: params[:quantity], completed: false)
 
-  redirect "lists/#{@list.id}/items"
+  redirect "user/lists/#{@list.id}/items"
 end
 
-get "/lists/:id/items" do
+get "/user/lists/:id/items" do
+  @title = "My List"
   @list = List.find(params[:id])
 
   @items = []
@@ -68,8 +86,6 @@ get "/lists/:id/items" do
     end
   end 
 
-  @title = "My Grocery List"
-
-  erb :"/lists/display_list"
+  erb :"user/lists/display_list"
 end
 
